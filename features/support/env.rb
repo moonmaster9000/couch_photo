ad6ddbdcafc0f0ac9b2b6275209eef83e287fab7
Akeem Adeniji <Adelanwa.A.Adeniji@aexp.com>
2012-01-18 11:27:24 -0500

    Updating test: clean up of temporary files
    Feature, step definition, and before hook updated
    to reflect the new behavior of cleaning up
    temporary files generated via Mime-magick/ImageMagick

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift "./lib"
 
+require 'cucumber/rspec/doubles'
 require 'couch_photo'
 require 'couchrest_model_config'
 
@@ -7,4 +8,8 @@ CouchRest::Model::Config.edit do
   database do
     default "http://admin:password@localhost:5984/couch_photo_test"
   end
+end
+
+Before('@file_stub') do
+  File.should_receive(:safe_unlink).twice
 end
